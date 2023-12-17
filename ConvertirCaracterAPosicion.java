7013cccb6d0dbb398f023d1565f87746f328b6fc
Vera Damian Luca <129334932+BLNDCD@users.noreply.github.com>
2023-12-17 01:50:42 -0300

    ConvertirCaracterAPosicion.java

---
+++ ConvertirCaracterAPosicion.java
@@ -0,0 +1,19 @@
+public class test {
+	
+	static void letraAPosicion(String a) {
+		a = a.toUpperCase();
+		for (int i = 0; i < a.length(); i++) {
+			char letra = a.charAt(i);
+			if(letra >='A' && letra <='Z') {
+				int posicion = letra- 'A' +1;
+				System.out.print(posicion + " ");
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String frase = "The sunset sets at twelve o' clock.";
+		letraAPosicion(frase);
+	}
+	
+}
